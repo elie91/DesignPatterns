9ac5ab6d1edead553c090e9fc7f38730918c61af
Bismuth <eliebismuth91@gmail.com>
2022-05-20 17:58:35 +0200

    up

---
+++ 3-Behavioral/Mediator/mediator.ts
@@ -0,0 +1,26 @@
+/*
+
+https://refactoring.guru/design-patterns/mediator
+
+Mediator is a behavioral design pattern that reduces coupling between components of a program by making them communicate indirectly, 
+through a special mediator object.
+
+The Mediator makes it easy to modify, extend and reuse individual components because they’re no longer dependent on the dozens of other classes.
+
+-> Use the Iterator pattern when your collection has a complex data structure under the hood, but you want to hide its complexity from clients (either for convenience or security reasons).
+-> Use the pattern to reduce duplication of the traversal code across your app.
+-> Use the Iterator when you want your code to be able to traverse different data structures or when types of these structures are unknown beforehand.
+
+Usage examples: 
+The most popular usage of the Mediator pattern in TypeScript code is facilitating communications between GUI components of an app. 
+The synonym of the Mediator is the Controller part of MVC pattern.
+
+
+Complexity: 2/3
+Popularity: 0/3
+*/
+
+
+namespace MediatorNamespace {
+
+}
